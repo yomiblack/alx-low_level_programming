d268c720abba63d5a84112726f450fc9a753575a
Abayomi <yomiblacck@gmail.com>
2022-09-09 11:56:30 -0700

    2-print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
