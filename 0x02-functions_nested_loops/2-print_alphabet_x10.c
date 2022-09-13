5fba55e77b680bc69a2fbe101f9c907c343fafec
Abayomi <yomiblacck@gmail.com>
2022-09-13 14:01:11 -0700

    2-print_alphabet_10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
- *
- *print_alphabet_x10 - prints alphabet 10 times
-
- * Return: Always 0.
- */
+* print_alphabet_x10 - Prints the alphabet 10 times.
+*
+* Return: Always 0
+*/
 void print_alphabet_x10(void)
 {
 	int i, j;
