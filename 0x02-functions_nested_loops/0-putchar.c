3b4fe55a209d51f865a65e21dfe08867b79461ed
Abayomi <yomiblacck@gmail.com>
2022-09-13 11:44:54 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * _putchar - defined function with return type int
+ *
+ * Return: Always 0.
+ */
+
+int _putchar(void);
+
+int main(void)
+{
+	char *a;
+
+	a = "_putchar";
+
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
+
+int _putchar(void)
+{
+	return (b);
+}
