b9332d983041b52809485cc705e0ea3fa55d3fe6
Abayomi <yomiblacck@gmail.com>
2022-09-13 17:14:55 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - main entry
+ *
+ * @n: value to compare
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (-n);
+}
