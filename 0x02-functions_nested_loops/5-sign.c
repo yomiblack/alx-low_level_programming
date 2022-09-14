26af0ccf3e5d174ad84788c1d5565d6898b85ae2
Abayomi <yomiblacck@gmail.com>
2022-09-13 17:02:09 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,13 +9,20 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		_putchar('+');
 	return (1);
+	}
 
 	if (n < 0)
+	{
 		_putchar('-');
 	return (-1);
+	}
 
+	else
+	{
 		_putchar('0');
 		return (0);
+	}
 }
