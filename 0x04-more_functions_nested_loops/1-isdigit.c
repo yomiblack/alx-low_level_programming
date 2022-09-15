ee81f4c27166c031fe4c616248dc7f2cdcfde7ea
Abayomi <yomiblacck@gmail.com>
2022-09-15 15:12:48 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - checks for digit
+ *
+ * @c: variable test
+ *
+ * Return: Always 0 (Success)
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
