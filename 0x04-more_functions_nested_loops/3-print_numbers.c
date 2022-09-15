ed8e8e053ae1ae2bb32d5fe2cc57ccdfaccfff3b
Abayomi <yomiblacck@gmail.com>
2022-09-15 15:40:48 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - printer positive numbers from 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int i,
+	    i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i = i + 1;
+	}
+	putchar('\n');
+}
