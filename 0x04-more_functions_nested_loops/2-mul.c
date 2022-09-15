ad7dc3bc2cf465bdc1ff8c8c21783e47a6985d22
Abayomi <yomiblacck@gmail.com>
2022-09-15 15:24:34 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplication function
+ *
+ * @a: variable test 1
+ *
+ * @b: variable test 2
+ *
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
