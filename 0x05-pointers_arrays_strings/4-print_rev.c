5b6652bb2ee8edbf58036a024bef8d23e5819959
Abayomi <yomiblacck@gmail.com>
2022-09-19 18:17:44 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - To print string in reverse.
+ * @s: pointer
+ * Return: (0)
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	for (len = len - 1; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
