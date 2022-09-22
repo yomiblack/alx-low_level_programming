cc279db459c7b2c3c4b0883761cfd4eeb1459abe
Abayomi <yomiblacck@gmail.com>
2022-09-22 11:31:37 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,21 +14,6 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, int j;
-
-	for (j = 0; dest[j] != '\0'; j++)
-	{}
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		dest[j + i] = src[i];
-	}
-	dest[j + i] =  '\0';
-
-	return (dest);
-
-}
-{
 	int i;
 	int j;
 
