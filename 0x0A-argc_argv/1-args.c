fe827d4a4f7ea325bb152a814235afbd6733c1b6
Abayomi <yomiblacck@gmail.com>
2022-09-30 15:09:49 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	(void)argv;
+
+	if (argc == 1)
+	{
+		putchar('0');
+		putchar('\n');
+	}
+	else
+	{
+		printf("%i\n", argc - 1);
+	}
+	return (0);
+}
