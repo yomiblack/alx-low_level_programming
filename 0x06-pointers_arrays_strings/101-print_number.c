3558e1f15eb4434aea79959eab42a33f10ca03eb
Abayomi <yomiblacck@gmail.com>
2022-10-18 01:02:11 -0700

    corrected programs

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,20 +6,22 @@
  */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int u, i;
 
 	if (n < 0)
 	{
-		n1 = -n;
+		u = -n;
 		_putchar('-');
 	}
 	else
 	{
-		n1 = n;
+		u = n;
 	}
 
-	if (n1 / 10)
-		print_number(n1 / 10);
-
-	_putchar((n1 % 10) + '0');
+	i = 1000000000;
+	do {
+		if (i <= u || i == 1)
+			_putchar(u / i % 10 + '0');
+		i /= 10;
+	} while (i != 0);
 }
